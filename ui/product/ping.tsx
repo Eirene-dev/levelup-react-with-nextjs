2aaa539d94d6806e9b001075f38c0a621367e3f3
Eirene <zu007kr@gmail.com>
2023-12-06 22:30:40 +0900

    route handler

--- ui/product/ping.tsx
+++ ui/product/ping.tsx
@@ -1,7 +1,7 @@
 export function Ping() {
   return (
     <span className="flex h-[11px] w-[11px]">
-      <span className="bg-vercel-pink absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
+      <span className="absolute inline-flex w-full h-full rounded-full opacity-75 bg-vercel-pink animate-ping"></span>
       <span className="bg-vercel-pink relative inline-flex h-[11px] w-[11px] rounded-full"></span>
     </span>
   );
